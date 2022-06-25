a03fee5891d862b0dc872345e124e0272512b07b
Simone Sada <si.sada93@gmail.com>
2022-06-25 22:32:19 +0200

    Setup i18n

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
+// use beforeEach route guard to set the language
+router.beforeEach((to, from, next) => {
+
+  // use the language from the routing param or default language
+  let language = to.params.lang
+  if(!language) {
+    language = 'en'
+  }
+
+  // set the current language for i18n.
+  i18n.locale = language
+  next()
+})
+
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import i18n from '../i18n'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: `/${i18n.locale}`
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/:lang',
+    component: {
+      render (c) { return c('router-view') }
+    },
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      }
+    ]
   }
 ]
 
