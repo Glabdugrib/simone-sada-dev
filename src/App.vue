20496b37a202fe9f0ad90a3accc1b2c585c35482
Simone Sada <si.sada93@gmail.com>
2022-06-25 22:48:35 +0200

    Import from previous repo

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,23 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "./components/Header.vue";
 
-nav {
-  padding: 30px;
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" scoped>
+@import "./assets/scss/_variables.scss";
+@import "./assets/scss/_mixins.scss";
+@import "./assets/scss/style.scss";
 </style>
