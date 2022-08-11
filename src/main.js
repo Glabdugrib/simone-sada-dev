4191a8767c31c0a67a559b7faaa174c0b1717787
Simone Sada <si.sada93@gmail.com>
2022-08-11 11:42:25 +0200

    Installed vue-particles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import VueParticles from 'vue-particles'
+
+Vue.use(VueParticles)
 
 Vue.config.productionTip = false
 
