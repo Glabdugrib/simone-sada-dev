7303137124ea2e1a20e2a9e0ecd3b246c0453cb8
Simone Sada <si.sada93@gmail.com>
2022-08-13 00:35:37 +0200

    Language select with custom style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,7 +23,7 @@
           <!-- <li :class="{'dark-mode-toggle': true, 'active': darkModeIsActive}">
             <button @click="toggleDarkMode()"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><title>Dark mode</title><path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 64V448C362 448 448 362 448 256C448 149.1 362 64 256 64z"/></svg></button>
           </li> -->
-          <li class="language-switcher">
+          <li>
             <LanguageSwitcher/>
           </li>
         </ol>
@@ -156,9 +156,9 @@ export default {
           margin: 0 3px;
           font-size: 15px;
 
-          &.language-switcher{
-            margin: 0 3px 0 6px;
-          }
+          // &.language-switcher{
+          //   margin: 0 3px 0 6px;
+          // }
 
           a {
             padding: 10px;
