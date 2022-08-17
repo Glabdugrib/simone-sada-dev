86d00ae8fb5e2a3b427026efc2ad1417a89baa32
Simone Sada <si.sada93@gmail.com>
2022-08-17 14:34:02 +0200

    Fixed name for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/simonesada-dev/',
+  publicPath: '/simone-sada-dev/',
 
   pluginOptions: {
     i18n: {
